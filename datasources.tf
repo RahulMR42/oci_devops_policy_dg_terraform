70fbc5198dfc21547162d9bef1bdd9f2b3b45d3b
fahdabidiroottenancy/Rahul <test@example.com>
2022-04-20 17:15:12 +0530

    tf test

--- datasources.tf
+++
@@ -1,5 +0,0 @@
-# Randoms
-resource "random_string" "random_id" {
-  length  = 4
-  special = false
-}
--- dgs.tf
+++ dgs.tf
@@ -1,6 +1,6 @@
 resource "oci_identity_dynamic_group" "devopsgroup1" {
   provider       = oci.home_region
-  name           = "${var.oci_dg_name}_${random_string.random_id}"
+  name           = ${var.oci_dg_name}
   description    = "DevOps deploy pipeline dynamic group"
   compartment_id = var.compartment_ocid
   matching_rule  = "ALL {resource.type = 'devopsdeploypipeline', resource.compartment.id = '${var.compartment_ocid}'}"
