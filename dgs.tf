68b4f5b93587413b0fb59dff5ee034f9db5a358f
fahdabidiroottenancy/Rahul <test@example.com>
2022-04-20 17:31:03 +0530

    tf test

--- dgs.tf
+++ dgs.tf
@@ -2,6 +2,6 @@ resource "oci_identity_dynamic_group" "devopsgroup1" {
   provider       = oci.home_region
   name           = var.oci_dg_name
   description    = "DevOps deploy pipeline dynamic group"
-  compartment_id = var.compartment_ocid
+  compartment_id = var.tenancy_ocid
   matching_rule  = "ALL {resource.type = 'devopsdeploypipeline', resource.compartment.id = '${var.compartment_ocid}'}"
 }
