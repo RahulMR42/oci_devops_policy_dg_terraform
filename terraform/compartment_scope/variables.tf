c5cf60aab8fad6434f854112bd52c29bf5db7baa
fahdabidiroottenancy/Rahul <test@example.com>
2022-04-20 18:18:33 +0530

    tf test

---
+++ terraform/compartment_scope/variables.tf
@@ -0,0 +1,20 @@
+variable oci_dg_name {
+  type        = string
+  default     = "oci_dg_sample"
+  description = "oci dg name"
+}
+
+variable compartment_ocid{
+    type     = string
+    default  = "ocid1.compartment.oc1..aaaaaaaagaqwgvp64baxtnfhdkeu45vdqpiuo2pqlxpeqzjf2mwh522zi53q"
+}
+
+variable region{
+    type = string
+    default = "us-ashburn-1"
+}
+
+variable tenancy_ocid{
+    type = string
+    default = "ocid1.tenancy.oc1..aaaaaaaasu7rvefmsyk5kqczfmdqi5clpddejfjk2attdqnk6sbk72wajq5q"
+}
